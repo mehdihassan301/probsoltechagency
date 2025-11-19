801f7e2018dae3ead798809346bde885e658b896
Mehdi Hassan <smartsameer015@gmail.com>
2025-11-19 21:00:58 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,17 +10,23 @@ export default defineConfig(({ mode }) => {
       port: 3000,
       host: '0.0.0.0',
     },
+
     plugins: [react()],
+
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
+
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('.', import.meta.url)),
       }
     },
-build: {
-  sourcemap: false
-}
+
+    build: {
+      sourcemap: false
+    }
+  };
+});
 
